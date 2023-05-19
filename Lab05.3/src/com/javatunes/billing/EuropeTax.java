6eb40ee4c22e2f748e98f6d9c1109fcf65cea183
Eric Phelps <Eric.Phelps@tlgcohort.com>
2023-05-19 10:39:16 -0400

    Imported the new DuckRace starter files

--- Lab05.3/src/com/javatunes/billing/EuropeTax.java
+++ Lab05.3/src/com/javatunes/billing/EuropeTax.java
@@ -10,11 +10,28 @@ package com.javatunes.billing;
 
 /**
  * European orders are taxed as follows:
- *  VAT is 17% of taxable amount.
- *  Luxury tax is an additional 25% on amount over $100.
- *  
+ * VAT is 17% of taxable amount.
+ * Luxury tax is an additional 25% on amount over $100.
+ *
  * TODO: implement this algorithm.
  */
-public class EuropeTax {
+public class EuropeTax implements TaxCalculator{
+    private static final double VAT = .17;
+    private static final double LUXURY_TAX_RATE = .25;
+    private static final double LUXURY_LIMIT = 100.00;
 
+
+    @Override
+    public double taxAmount(double taxable) {
+        double result = 0.0;
+
+        if (taxable > LUXURY_LIMIT){
+            result = (((taxable - 100) * LUXURY_TAX_RATE) + (taxable * VAT));
+        }
+        else{
+            result = (taxable * VAT);
+        }
+
+        return result;
+    }
 }
