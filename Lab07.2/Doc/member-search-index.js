259df886a624074ede619bf9d36964329b63937c
Eric Phelps <Eric.Phelps@tlgcohort.com>
2023-05-22 18:19:01 -0400

    Lab7.2 completed, custom annotation practice

---
+++ Lab07.2/Doc/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.fastjava.annotation","c":"Fast","l":"boostFactor()"},{"p":"com.fastjava.annotation","c":"Fast","l":"description()"},{"p":"com.fastjava.annotation","c":"Fast","l":"log()"}]
---
+++ Lab07.2/src/com/javatunes/personnel/EmployeeClient.java
@@ -0,0 +1,10 @@
+package com.javatunes.personnel;
+
+class EmployeeClient {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee();
+
+        emp1.work();
+        emp1.pay();
+    }
+}
