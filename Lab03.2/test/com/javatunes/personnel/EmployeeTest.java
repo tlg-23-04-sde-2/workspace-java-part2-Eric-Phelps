add29e94316cb16ca72733a672c4ca45de9f4641
Eric Phelps <Eric.Phelps@tlgcohort.com>
2023-05-22 12:16:48 -0400

    employeeTest with dummyEmployee

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -16,8 +16,18 @@ public class EmployeeTest
     @Before
     public void setUp()
     {
-       emp1 = new SalariedEmployee("John", Date.valueOf("2000-01-01"));
-       emp2 = new SalariedEmployee("John", Date.valueOf("2000-01-01"));
+        emp1 =  new Employee("John", Date.valueOf("2000-01-01")) {
+            public double pay() {return 0;}
+            public double payTaxes() {return 0;}
+        };
+
+        emp2 =  new Employee("John", Date.valueOf("2000-01-01")) {
+            public double pay() {return 0;}
+            public double payTaxes() {return 0;}
+        };
+
+//       emp1 = new DummyEmployee("John", Date.valueOf("2000-01-01"));
+//       emp2 = new DummyEmployee("John", Date.valueOf("2000-01-01"));
     }
 
     @Test
@@ -28,7 +38,7 @@ public class EmployeeTest
     }
 
     @Test
-    public void equals_shouldReturnFalse_sameName_differentHireDate()
+    public void equals_shouldReturnFalse_sameName_diffHireDate()
     {
         emp2.setHireDate(Date.valueOf("2001-10-10"));
         assertNotEquals(emp1,emp2);
@@ -39,5 +49,24 @@ public class EmployeeTest
     {
         assertEquals(emp1,emp2); // this does an equals() check for objects
         //assertTrue(emp1.equals(emp2));
+    }
+
+    //INNER_CLASS
+    private class DummyEmployee extends Employee {
+
+        //constructor
+        public DummyEmployee(String name, Date hireDate) {
+            super(name, hireDate);
+        }
+
+        @Override
+        public double pay() {
+            return 0.0;
+        }
+
+        @Override
+        public double payTaxes() {
+            return 0.0;
+        }
     }
 }
